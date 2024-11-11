7a7c24e07878c15e4524be4251e9efa1c3694e9d
Cristian <cristian.cantor01@uptc.com>
2024-11-10 22:44:38 -0500

    first commit

---
+++ src/main/java/com/uptc/frw/caso_grpc/CasoGRpcApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.caso_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoGRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasoGRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/caso_grpc/CasoGRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.caso_grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoGRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
